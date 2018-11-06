2b287491b7a9b59f5c3e09054100391ffae38708
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2018-11-06 19:16:48 +0100

    update

--- lib/zmcli.rb
+++ lib/zmcli.rb
@@ -9,25 +9,27 @@ module Zmcli
     opt_parser = OptionParser.new do |opt|
       opt.banner = "Usage: COMMAND [OPTIONS]"
 
-      opt.on '--reindex all', 'Reindex all accounts' do |arg|
-        accounts = []
-        stdin, stdout, stderr = Open3.popen3("/opt/zimbra/bin/zmprov -l gaa")
-        gaa = stdout.read
-        accounts = gaa.split("\n")
-        accounts.each do |a|
-          puts "Reindexing #{a}"
-          system("/opt/zimbra/bin/zmprov rim #{a} start")
-          puts "Finished Reindexing of #{a}"
-        end
+      opt.on '--reindex ACCOUNT', 'Reindex accounts' do |arg|
+        options[:reindex] = arg
       end
 
-      opt.on '--reindex', 'Reindex a single account' do |arg|
-        puts   "Reindexing #{arg}"
-        system "/opt/zimbra/bin/zmprov rim #{arg} start"
-        puts   "Finished Reindexing of #{arg}"
-      end
+    end
+    opt_parser.parse!
 
+    if options[:reindex] == "all"
+      accounts = []
+      stdin, stdout, stderr = Open3.popen3("/opt/zimbra/bin/zmprov -l gaa")
+      gaa = stdout.read
+      accounts = gaa.split("\n")
+      accounts.each do |a|
+        puts "Reindexing #{a}"
+        system("/opt/zimbra/bin/zmprov rim #{a} start")
+        puts "Finished Reindexing of #{a}"
+      end
+    else
+      puts   "Reindexing #{arg}"
+      system "/opt/zimbra/bin/zmprov rim #{arg} start"
+      puts "Finished Reindexing of #{arg}"
+    end
   end
-  opt_parser.parse!
-end
 end
