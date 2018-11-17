4b593a418ed41629787ec227492f0dee6d29c3a8
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2018-11-17 15:51:57 +0100

    start using constants

--- lib/zmcli/account.rb
+++ lib/zmcli/account.rb
@@ -1,32 +1,35 @@
 module Zmcli
+
+  ZMPROV = '/opt/zimbra/bin/zmprov'
+
   class Account
     def initialize(account = nil)
       @account = account
     end
     def list_all
-      stdout, status = Open3.capture2("/opt/zimbra/bin/zmprov -l gaa")
+      stdout, status = Open3.capture2(ZMPROV, '-l', 'gaa')
       stdout.read.split("\n")
     end
     def reindex
       puts   "Reindexing #{@account}"
-      system("/opt/zimbra/bin/zmprov rim #{@account} start")
+      system(ZMPROV, 'rim', @account, 'start')
       puts "Finished Reindexing of #{@account}"
     end
     def backup
       puts "Backing up #{@account}"
       after_string = '"' + "/?fmt=tgz" + '"'
-      system("/opt/zimbra/bin/zmmailbox -z -m #{@account} getRestURL #{after_string} > #{@account}.tar.gz")
+      system(ZMPROV, '-z', '-m', @account, 'getRestURL', after_string, '>', "#{@account}.tar.gz")
     end
     def restore
       puts "Restoring #{@account}"
       after_string = '"' + "/?fmt=tgz&resolve=skip" + '"'
-      system("/opt/zimbra/bin/zmmailbox -z -m #{@account} postRestURL #{after_string} #{@account}.tar.gz")
+      system(ZMPROV, '-z', '-m', @account, 'postRestURL', after_string, "#{@account}.tar.gz")
     end
     def backup_last_month
       last_month = Time.now.to_date.prev_month.strftime '%m/%d/%Y'
       after_string = '"' + "//?fmt=tgz&query=after:#{last_month}" + '"'
       puts "Backing up last month of #{@account}"
-      system("/opt/zimbra/bin/zmmailbox -z -m #{@account} getRestURL #{after_string} > #{@account}.tar.gz")
+      system(ZMPROV, '-z', '-m', @account, 'getRestURL', after_string, '>', "#{@account}.tar.gz")
     end
   end
 end
