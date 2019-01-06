065c89ef6f1cca73faf774b43b591b7f30711f3a
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2019-01-06 22:47:40 +0100

    switch to ARGV

--- lib/zmcli.rb
+++ lib/zmcli.rb
@@ -9,127 +9,110 @@ require_relative 'zmcli/quota.rb'
 
 module Zmcli
   class Main
+
+    cmd=ARGV.shift
+
     options = {}
 
-    opt_parser = OptionParser.new do |opt|
-      opt.banner = "Usage: COMMAND [OPTIONS]"
-
-      opt.on '--reindex ACCOUNT', 'Reindex accounts' do |arg|
-        options[:reindex] = arg
-      end
-      opt.on '--backup-last-month-account ACCOUNT', 'Backup account for period of last month' do |arg|
-        options[:blma] = arg
-      end
-      opt.on '--backup ACCOUNT', 'Backup account.' do |arg|
-        options[:backup_account] = arg
-      end
-      opt.on '--backup-accounts-for-domain DOMAIN', 'Backup account for period of last month' do |arg|
-        options[:bafd] = arg
-      end
-      opt.on '--restore ACCOUNT', 'Restore account.' do |arg|
-        options[:restore_account] = arg
-      end
-      opt.on '--increase-mail-quota-for-account ACCOUNT', 'Increase mail quota for account' do |arg|
-        options[:imqfa] = arg
-      end
-      opt.on '--increase-mail-quota-for-all-domain-accounts DOMAIN', 'Increase mail quota for all accounts under a domain.' do |arg|
-        options[:imqfada_domain] = arg
-      end
-      opt.on '--make-admin ACCOUNT, DOMAIN', 'Backup account for period of last month' do |arg|
-        options[:makeadmin_account] = arg[0]
-        options[:makeadmin_domain] = arg[1]
+    ARGV.each do |a|
+      key, value = a.split("=")
+      case key
+      when "--domain", "-d"
+        options[:domain]=value
+      when "--account", "-a"
+        options[:account]=value
       end
     end
 
-    opt_parser.parse!
-
-    # Reindex All Acounts
-    if options[:reindex] == "all"
-      accounts = []
-      accounts = Account.new().list_all
-      accounts.each do |a|
-        account = Account.new(a)
-        account.reindex
+    case cmd
+    when "reindex"
+      # Reindex All Acounts
+      if options[:account] == "all"
+        accounts = []
+        accounts = Account.new().list_all
+        accounts.each do |a|
+          account = Account.new(a)
+          account.reindex
+        end
+      else
+        # Reindex a single account
+        if options[:account]
+          account = Account.new(options[:reindex])
+          account.reindex
+        end
       end
-    else
-      # Reindex a single account
-      if options[:reindex]
-        account = Account.new(options[:reindex])
-        account.reindex
+    when "backup-last-month"
+      # Backup Last Month for all Accounts
+      if options[:account] == "all"
+        accounts = []
+        accounts = Account.new().list_all
+        accounts.each do |a|
+          account = Account.new(a)
+          account.backup_last_month
+        end
+      else
+        if options[:account]
+          account = Account.new(options[:account])
+          account.backup_last_month
+        end
       end
-    end
-
-    # Backup Last Month for all Accounts
-    if options[:blma] == "all"
-      accounts = []
-      accounts = Account.new().list_all
-      accounts.each do |a|
-        account = Account.new(a)
-        account.backup_last_month
-      end
-    else
-      if options[:blma]
-        account = Account.new(options[:blma])
-        account.backup_last_month
-      end
-    end
-
-    # Backup Complete Account
-    if options[:backup_account]
-      account = Account.new(options[:backup_account])
-      account.backup
-    end
-
-    # Backup Accounts For Domain
-    if options[:bafd]
-      accounts = []
-      domain = Domain.new(options[:bafd])
-      accounts = domain.list_all_accounts
-      accounts.each do |a|
-        account = Account.new(a)
+    when "backup"
+      # Backup Complete Account
+      if options[:account]
+        account = Account.new(options[:account])
         account.backup
       end
-    end
-
-    # Restore Complete Account
-    if options[:restore_account]
-      account = Account.new(options[:restore_account])
-      account.restore
-    end
-
-    # Increase Mail Quota For Account
-    if options[:imqfa]
-      current_mail_quota = Quota.new(options[:imqfa]).get_quota_usage_for_account
-      new_mail_quota = current_mail_quota.to_i + 943718400
-      puts "Current mail quota is:"
-      Quota.new(options[:imqfa]).get_current_mail_quota
-      Quota.new(options[:imqfa], new_mail_quota).get_current_mail_quota
-      puts "New mail quota is:"
-      Quota.new(options[:imqfa]).get_current_mail_quota
-    end
-
-    # Increase Mail Quota For All Domain Accounts
-    if options[:imqfada_domain]
-      accounts = []
-      domain = Domain.new(options[:imqfada_domain])
-      accounts = domain.list_all_accounts
-      accounts.each do |a|
-        account = Quota.new(a)
-        account.get_current_mail_quota
-        new_mail_quota = current_mail_quota.to_i + 943718400
-        puts "Increasing mail quota for account #{a}"
-        puts "Current mail quota is:"
-        account.get_current_mail_quota
-        Quota.new(a, new_mail_quota).get_current_mail_quota
-        puts "New mail quota is:"
-        account.get_current_mail_quota
+    when "backup-accounts-for-domain"
+      # Backup Accounts For Domain
+      if options[:bafd]
+        accounts = []
+        domain = Domain.new(options[:bafd])
+        accounts = domain.list_all_accounts
+        accounts.each do |a|
+          account = Account.new(a)
+          account.backup
+        end
       end
-    end
-
-    # Grant Admin Privileges for a Domain
-    if options[:makeadmin_account] && options[:makeadmin_domain]
-      admin = MakeAdmin.new(options[:makeadmin_account],options[:makeadmin_domain])
-      admin.make_admin
+    when "--restore"
+      # Restore Complete Account
+      if options[:restore_account]
+        account = Account.new(options[:restore_account])
+        account.restore
+      end
+    when "--increase-mail-quota"
+      # Increase Mail Quota For Account
+      if options[:account]
+        current_mail_quota = Quota.new(options[:account]).get_quota_usage_for_account
+        new_mail_quota = current_mail_quota.to_i + 943718400
+        puts "Current mail quota is:"
+        Quota.new(options[:account]).get_current_mail_quota
+        Quota.new(options[:account], new_mail_quota).get_current_mail_quota
+        puts "New mail quota is:"
+        Quota.new(options[:account]).get_current_mail_quota
+      end
+      # Increase Mail Quota For All Domain Accounts
+      if options[:domain]
+        accounts = []
+        domain = Domain.new(options[:domain])
+        accounts = domain.list_all_accounts
+        accounts.each do |a|
+          account = Quota.new(a)
+          account.get_current_mail_quota
+          new_mail_quota = current_mail_quota.to_i + 943718400
+          puts "Increasing mail quota for account #{a}"
+          puts "Current mail quota is:"
+          account.get_current_mail_quota
+          Quota.new(a, new_mail_quota).get_current_mail_quota
+          puts "New mail quota is:"
+          account.get_current_mail_quota
+        end
+      end
+    when "make-admin"
+      # Grant Admin Privileges for a Domain
+      if options[:account] && options[:domain]
+        admin = MakeAdmin.new(options[:account],options[:domain])
+        admin.make_admin
+      end
     end
 
   end
