4957eaa70e34ef0129273afd9ca6c3f929816c53
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2019-01-07 22:16:23 +0100

    fix backup export

--- lib/zmcli/account.rb
+++ lib/zmcli/account.rb
@@ -16,19 +16,18 @@ module Zmcli
     end
     def backup
       puts "Backing up #{@account}"
-      after_string = '"' + "/?fmt=tgz" + '"'
-      system("#{ZMPATH}zmprov", '-z', '-m', @account, 'getRestURL', after_string, '>', "#{@account}.tar.gz")
+      system("#{ZMPATH}zmmailbox", '-z', '-m', @account, 'getRestURL', "//?fmt=tgz", '>', "#{@account}.tar.gz")
     end
     def restore
       puts "Restoring #{@account}"
-      after_string = '"' + "/?fmt=tgz&resolve=skip" + '"'
-      system("#{ZMPATH}zmprov", '-z', '-m', @account, 'postRestURL', after_string, "#{@account}.tar.gz")
+      after_string = '"' + "//?fmt=tgz&resolve=skip" + '"'
+      system("#{ZMPATH}zmmailbox", '-z', '-m', @account, 'postRestURL', after_string, "#{@account}.tar.gz")
     end
     def backup_last_month
       last_month = Time.now.to_date.prev_month.strftime '%m/%d/%Y'
       after_string = '"' + "//?fmt=tgz&query=after:#{last_month}" + '"'
       puts "Backing up last month of #{@account}"
-      system("#{ZMPATH}zmprov", '-z', '-m', @account, 'getRestURL', after_string, '>', "#{@account}.tar.gz")
+      system("#{ZMPATH}zmmailbox", '-z', '-m', @account, 'getRestURL', after_string, '>', "#{@account}.tar.gz")
     end
   end
 end
