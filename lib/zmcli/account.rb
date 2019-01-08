e1b7c3e7319e715341a997de0c8cee03e2d40abd
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2019-01-08 16:04:32 +0100

    add some error checks for methods

--- lib/zmcli/account.rb
+++ lib/zmcli/account.rb
@@ -11,16 +11,27 @@ module Zmcli
     end
     def reindex
       puts   "Reindexing #{@account}"
-      system("#{ZMPATH}zmprov", 'rim', @account, 'start')
-      puts "Reindexing of #{@account} is finished"
+      if system("#{ZMPATH}zmprov", 'rim', @account, 'start')
+        puts "Reindexing of #{@account}  was successful."
+      else
+        puts "Reindexing of #{@account} failed"
+      end
     end
     def backup
       puts "Backing up #{@account}"
-      system("#{ZMPATH}zmmailbox", '-z', '-m', @account, 'getRestURL', '-o', "#{@account}.tar.gz", "//?fmt=tgz")
+      if system("#{ZMPATH}zmmailbox", '-z', '-m', @account, 'getRestURL', '-o', "#{@account}.tar.gz", "//?fmt=tgz")
+        puts "Backing up #{@account} was successful."
+      else
+        puts "Backing up #{@account}"
+      end
     end
     def restore
       puts "Restoring #{@account}"
-      system("#{ZMPATH}zmmailbox", '-z', '-m', @account, 'postRestURL', '-o', "#{@account}.tar.gz", "//?fmt=tgz&resolve=skip")
+      if system("#{ZMPATH}zmmailbox", '-z', '-m', @account, 'postRestURL', '-o', "#{@account}.tar.gz", "//?fmt=tgz&resolve=skip")
+        puts "Restore of #{@account} was successful."
+      else
+        puts "Restore of #{@account} failed."
+      end
     end
     def backup_last_month
       last_month = Time.now.to_date.prev_month.strftime '%m/%d/%Y'
