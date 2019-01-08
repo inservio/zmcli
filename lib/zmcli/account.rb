90f7116bcd7181c1cb33555d97bdac0118f7920d
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2019-01-08 15:23:57 +0100

    remove after_string variables

--- lib/zmcli/account.rb
+++ lib/zmcli/account.rb
@@ -24,9 +24,8 @@ module Zmcli
     end
     def backup_last_month
       last_month = Time.now.to_date.prev_month.strftime '%m/%d/%Y'
-      after_string = "//?fmt=tgz&query=after:#{last_month}"
       puts "Backing up last month of #{@account}"
-      system("#{ZMPATH}zmmailbox", '-z', '-m', @account, 'getRestURL', '-o', "#{@account}.tar.gz", after_string)
+      system("#{ZMPATH}zmmailbox", '-z', '-m', @account, 'getRestURL', '-o', "#{@account}.tar.gz", "//?fmt=tgz&query=after:#{last_month}")
     end
   end
 end
